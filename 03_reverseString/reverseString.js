09113131c362acf4826133a3bb1e57aef4b79e1c
CodeNeBo <nechitaylob@gmail.com>
2022-07-28 17:17:20 +0300

    Done! Third task passed all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    let splitText = text.split("");
+    let revText = splitText.reverse(); 
+    let newText = revText.join("");
 
+    return newText; 
 };
 
 // Do not edit below this line
