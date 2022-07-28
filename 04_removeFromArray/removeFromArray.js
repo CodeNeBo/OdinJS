29a14f555511830b83750ed43d880fcbb5603d12
CodeNeBo <nechitaylob@gmail.com>
2022-07-28 18:55:35 +0300

    Passed tests. Have to remember this solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    const array = args[0];
+    const newArray = [];
 
+    array.forEach((itemFromFuncInput) => {
+        if (!args.includes(itemFromFuncInput)) {
+            newArray.push(itemFromFuncInput);
+        }
+    });
+
+    return newArray;
 };
 
 // Do not edit below this line
