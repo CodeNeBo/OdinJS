0ecd2ae94751ec98dc92fdc05a5db90ccf0797c3
CodeNeBo <nechitaylob@gmail.com>
2022-07-29 12:16:52 +0300

    Done. Passed all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fare) {
+  let tempCelsius = (fare - 32) * (5/9)
+  let celsius = Math.round(tempCelsius * 10) / 10
+  return celsius;
 };
 
-const ctof = function() {
-
+const ctof = function(cels) {
+  let tempFahrenheit = (cels * (9/5) + 32)
+  let fahrenheit = Math.round(tempFahrenheit * 10) / 10
+  return fahrenheit;
 };
 
 // Do not edit below this line
